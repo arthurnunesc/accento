5db799075b14e1eb31d0b08bd2aa2d77150a2e8b
Arthur Nunes <arthurnunesc@protonmail.com>
2022-05-13 22:35:35 -0300

    Changed variable and function names to fit conventions
    And commented the code thoroughly

--- main.py
+++ main.py
@@ -1,102 +1,132 @@
 import string
 from collections import Counter
 
-def analyseText(textToBeAnalysed):
+
+def analyze_text(text_to_be_analyzed):
     alpha = list(string.ascii_lowercase)
-    alpha.insert(14, "ñ")
-    alpha.insert(3, "ç")
-    alphaIncidence = []
-    alphaPercentages = []
+
+    alpha_apostrophe = ["á", "é", "í", "ó", "ú", "ç"]
+    alpha_double_quotes = ["ä", "ë", "ï", "ö", "ü", "ÿ"]
+    alpha_grave = ["à", "è", "ì", "ò", "ù"]
+    alpha_tilde = ["ã", "ẽ", "ĩ", "õ", "ũ", "ñ"]
+    alpha_circumflex = ["â", "ê", "î", "ô", "û"]
+    alpha_alt_gr = ["ø", "å", "ß", "µ", "ð", "þ", "œ", "æ"]
+
+    alpha_incidences = []
+    alpha_percentages = []
     digits = list(string.digits)
-    digitsIncidence = []
-    digitsPercentages = []
-    punctuation = list(string.punctuation)
-    punctuationIncidence = []
-    punctuationPercentages = []
+    digits_incidences = []
+    digits_percentages = []
+    punctuation_marks = list(string.punctuation)
+    punctuation_marks_incidences = []
+    punctuation_marks_percentages = []
+    print(alpha)
+    print(digits)
+    print(punctuation_marks)
 
-    textToBeAnalysedWithoutSpaces = textToBeAnalysed.replace(" ", "")
-    lengthOfTextToBeAnalysedWithoutSpaces = len(textToBeAnalysedWithoutSpaces)
-    textToBeAnalysedWithoutSpacesAndAllLowerCase = textToBeAnalysedWithoutSpaces.lower()
+    # Deletes every single space in the input
+    text_to_be_analyzed_without_spaces = text_to_be_analyzed.replace(" ", "")
+    # Length of the input without the spaces
+    length_of_text_to_be_analyzed_without_spaces = len(
+        text_to_be_analyzed_without_spaces)
+    # Makes all the input lowercase
+    text_to_be_analyzed_without_spaces_and_all_lower_case = text_to_be_analyzed_without_spaces.lower()
 
+    alpha_counter = Counter()
+    # Separates the input in lines
+    for line in text_to_be_analyzed_without_spaces_and_all_lower_case:
+        # Iterates through every letter in alpha
+        for letter in alpha:
+            if letter in line:
+                alpha_counter.update(line)
+            elif letter not in line and letter not in alpha_counter.keys():
+                # If the letter is not present in the line and wasn't present in the lines before, declares its value as zero so it appears in the final counter and doesn't mess up and the percentage count
+                alpha_counter[letter] = 0
+    for value in alpha_counter.values():
+        # Adds how many times each letter appeared to a list
+        alpha_incidences.append(value)
+    for incidence in alpha_incidences:
+        # Calculates the percentage of each letter using the times it appeared and the total length of the text
+        alpha_percentages.append(
+            round(incidence / length_of_text_to_be_analyzed_without_spaces * 100, 4))
+    alpha_dictionary = dict(zip(alpha, alpha_percentages))
 
-    alphaCounter = Counter()
-    for line in textToBeAnalysedWithoutSpacesAndAllLowerCase:
-        for each in alpha:
+    digits_counter = Counter()
+    for line in text_to_be_analyzed_without_spaces_and_all_lower_case:
+        for digit in digits:
+            if digit in line:
+                digits_counter.update(line)
+            elif digit not in line and digit not in digits_counter.keys():
+                digits_counter[digit] = 0
+    for each in digits_counter.values():
+        digits_incidences.append(each)
+    for each in digits_incidences:
+        digits_percentages.append(
+            round(each / length_of_text_to_be_analyzed_without_spaces * 100, 4))
+    digits_dictionary = dict(zip(digits, digits_percentages))
+
+    punctuation_marks_counter = Counter()
+    for line in text_to_be_analyzed_without_spaces_and_all_lower_case:
+        for each in punctuation_marks:
             if each in line:
-               alphaCounter.update(line)
-            elif each not in line and each not in alphaCounter.keys():
-                alphaCounter[each] = 0
-    for each in alphaCounter.values():
-        alphaIncidence.append(each)
-    for each in alphaIncidence:
-        alphaPercentages.append(round(each / lengthOfTextToBeAnalysedWithoutSpaces * 100, 4))
-    alphaDictionary = dict(zip(alpha, alphaPercentages))
+                punctuation_marks_counter.update(line)
+            elif each not in line and each not in punctuation_marks_counter.keys():
+                punctuation_marks_counter[each] = 0
+    for each in punctuation_marks_counter.values():
+        punctuation_marks_incidences.append(each)
+    for each in punctuation_marks_incidences:
+        punctuation_marks_percentages.append(
+            round(each / length_of_text_to_be_analyzed_without_spaces * 100, 4))
+    punctuation_marks_dictionary = dict(
+        zip(punctuation_marks, punctuation_marks_percentages))
+
+    everything_dictionary = alpha_dictionary.copy()
+    everything_dictionary.update(digits_dictionary)
+    everything_dictionary.update(punctuation_marks_dictionary)
+    sorted_everything_dictionary = sorted(
+        everything_dictionary.items(), key=lambda x: x[1], reverse=True)
+
+    return everything_dictionary, sorted_everything_dictionary
 
 
-    digitsCounter = Counter()
-    for line in textToBeAnalysedWithoutSpacesAndAllLowerCase:
-        for each in digits:
-            if each in line:
-               digitsCounter.update(line)
-            elif each not in line and each not in digitsCounter.keys():
-                digitsCounter[each] = 0
-    for each in digitsCounter.values():
-        digitsIncidence.append(each)
-    for each in digitsIncidence:
-        digitsPercentages.append(round(each / lengthOfTextToBeAnalysedWithoutSpaces * 100, 4))
-    digitsDictionary = dict(zip(digits, digitsPercentages))
-    
+# Names of the files containing the texts to be analyzed
+files = ['books/ptBrDomCasmurro.txt']
 
-    punctuationCounter = Counter()
-    for line in textToBeAnalysedWithoutSpacesAndAllLowerCase:
-        for each in punctuation:
-            if each in line:
-                punctuationCounter.update(line)
-            elif each not in line and each not in punctuationCounter.keys():
-                punctuationCounter[each] = 0
-    for each in punctuationCounter.values():
-        punctuationIncidence.append(each)
-    for each in punctuationIncidence:
-        punctuationPercentages.append(round(each / lengthOfTextToBeAnalysedWithoutSpaces * 100, 4))
-    punctuationDictionary = dict(zip(punctuation, punctuationPercentages))
+texts = []
 
-    everythingDictionary = alphaDictionary.copy()
-    everythingDictionary.update(digitsDictionary)
-    everythingDictionary.update(punctuationDictionary)
-    sortedEverythingDictionary = sorted(everythingDictionary.items(), key=lambda x: x[1], reverse=True)
-
-    return everythingDictionary, sortedEverythingDictionary
-
-# Nomes dos arquivos nos quais os textos se encontram, em plain text
-arquivos = ['books\engDracula.txt', 'books\engFrankenstein.txt', 'books\esEsElCamino.txt', 'books\esPeTupacAmaru.txt', 'books\ptBrDomCasmurro.txt', 'books\ptPtOsLusiadas.txt', 'books\catElsCaminsDelParadisPerdut.txt', 'books\catEnPereIAltresContes.txt']
-
-textos = []
-
-# Armazena cada um dos textos no vetor textos
-for each in arquivos:
+# Stores every text in the files in the list texts
+for each in files:
     f = open(each, encoding="utf8")
-    textos.append(f.read())
+    texts.append(f.read())
     f.close()
 
-dicionarios = []
-dicionariosOrganizados = []
+dictionaries = []
+dictionaries_organizados = []
 
-# Aplica a função analyseText a todos os textos armazenados e armazena os resultados no vetor dicionarios
-for each in textos:
-    dicionarios.append(analyseText(each)[0])
-    # dicionariosOrganizados.append(analyseText(each)[1])
+# Applies the function analyze_text to all the texts and then stores the results in the list dictionaries
+for each in texts:
+    dictionaries.append(analyze_text(each)[0])
+    # dictionaries_organizados.append(analyze_text(each)[1])
 
-dicionarioGeral = dicionarios[0].copy()
+dictionaries_geral = dictionaries[0].copy()
 
-# Soma todos os dicionários no vetor dicionarios e os adiciona num único dicionário chamado dicionarioGeral
-for index in range(len(dicionarios)):
+# Soma todos os dicionários no vetor dictionaries e os adiciona num único dicionário chamado dictionaries_geral
+for index in range(len(dictionaries)):
     if index != 0:
         # Por usar counter, descarta todos os caracteres que tem porcentagem(contagem) igual a 0
-        dicionarioGeral = Counter(dicionarioGeral) + Counter(dicionarios[index])
+        dictionaries_geral = Counter(dictionaries_geral) + \
+            Counter(dictionaries[index])
 
 # Divide a soma de todos os dicionários pelo número total de dicionários, efetivamente, calculando a média deles
-dicionarioGeral = {k: round(v / len(dicionarios), 4) for k, v in dicionarioGeral.items()}
+dictionaries_geral = {k: round(v / len(dictionaries), 4)
+                      for k, v in dictionaries_geral.items()}
 
-# Organiza o dicionarioGeral em ordem decrescente
-dicionarioGeralOrganizado = sorted(dicionarioGeral.items(), key=lambda x: x[1], reverse=True)
-print(dicionarioGeralOrganizado)
+# Organiza o dictionaries_geral em ordem decrescente
+dictionaries_geral_organizado = sorted(
+    dictionaries_geral.items(), key=lambda x: x[1], reverse=True)
+print(dictionaries_geral_organizado)
+
+# string = "ppkæø°domal"
+# counter = Counter()
+# counter.update(string)
+# print(counter)
